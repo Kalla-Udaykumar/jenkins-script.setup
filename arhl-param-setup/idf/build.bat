3b77c1f91c0e1d2b2f81f5e8ec21d425c66a6b7d
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2024-05-14 14:48:52 +0530

    Update build.bat

--- arhl-param-setup/idf/build.bat
+++ arhl-param-setup/idf/build.bat
@@ -2,4 +2,4 @@ echo workspace is: %WORKSPACE%
 echo working_dir is: %WORKING_DIR%
 copy /Y %WORKSPACE%\\abi\\BIOS_Manifest.json %WORKING_DIR%\\IFWI_Automation\\ARL\\ARL_H\\INPUT\\STATIC\\IFWI_UNZIP
 cd %WORKING_DIR%\\IFWI_Automation\\ARL\\test\\
-py ARL_one_darzi.py ARL_H
+py ARL_one_darzi.py ARL_H ifwi_arl_h_a0_pp_release_2024ww17.2.02 Pre_Production
