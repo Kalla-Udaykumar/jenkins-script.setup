54aab5f09a4b9f9be7d5f9ac2a51841caebdd941
mutra-vamsi <99331612+mutra-vamsi@users.noreply.github.com>
2024-07-15 11:58:32 +0530

    Update dwnld.py

--- asl-unified-odm/idf/dwnld.py
+++ asl-unified-odm/idf/dwnld.py
@@ -18,18 +18,18 @@ print(WORKING_DIR)
 print(WORKSPACE)
 
 s = requests.Session()
-r = s.get('https://ubit-artifactory-ba.intel.com/artifactory/one-windows-local/Submissions/ifwi/', auth=HTTPBasicAuth(username, password), stream=True)
+r = s.get('https://ubit-artifactory-or.intel.com/artifactory/qsr-or-local/Submissions/ifwi/', auth=HTTPBasicAuth(username, password), stream=True)
 soup = BeautifulSoup(r.text, 'html.parser')
 
 links = soup.find_all("a")
 # ifwi_version 
 for link in links:
-        if ("adl_n_win11_mr_blue_ifwi_2022_ww52_4_01" in str(link)) and ("_v" not in str(link)):
+        if ("adl_n_win11-ipu_24_3_ifwi_2024" in str(link)) and ("_v" not in str(link)):
                     #print(link.get('href'))
                     IFWI_VERSION = link.get('href')[0:-1]
 
 # bios_version					
-with open(WORKING_DIR + "\\IFWI_Automation\\Builder\\bios_version_internal.txt", "r") as f:
+with open(WORKING_DIR + "\\IFWI_Automation\\Unified_IFWI\\Builder\\bios_version_internal.txt", "r") as f:
     for line in f:
         BIOS_VERSION = line
 
@@ -46,7 +46,7 @@ with open(bios_version_file_path, "w") as f:
 print("IFWI_VERSION is :" + IFWI_VERSION)
 print("py ASL_IFWI_download_unzip.py " + IFWI_VERSION + " Pre_Production" + " Production")
 
-os.chdir(WORKING_DIR + "\IFWI_Automation\Builder")
+os.chdir(WORKING_DIR + "\\IFWI_Automation\\Unified_IFWI\\Builder")
 dwnld_return_value = os.system("py ASL_IFWI_download_unzip.py " + IFWI_VERSION + " Pre_Production" + " Production")
 if dwnld_return_value != 0:
 	sys.exit(-1)
