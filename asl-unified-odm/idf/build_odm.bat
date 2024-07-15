10398e4f30b2308df158534a06c4fc26c260c0ee
mutra-vamsi <99331612+mutra-vamsi@users.noreply.github.com>
2024-07-15 11:55:38 +0530

    Update build_odm.bat

--- asl-unified-odm/idf/build_odm.bat
+++ asl-unified-odm/idf/build_odm.bat
@@ -1,7 +1,5 @@
 echo workspace is: %WORKSPACE%
 echo working_dir is: %WORKING_DIR%
-cd %WORKING_DIR%\\IFWI_Automation\\Builder
-py ASL_IFWI_download_unzip.py %IFWI_VERSION% Pre_Production Production
-copy /Y %WORKSPACE%\\abi\\BIOS_Manifest.json %WORKING_DIR%\\IFWI_Automation\\ASL\\INPUT\\STATIC\\IFWI_UNZIP
-cd %WORKING_DIR%\\IFWI_Automation\\Builder
+copy /Y %WORKSPACE%\\abi\\BIOS_Manifest.json %WORKING_DIR%\\IFWI_Automation\\Unified_IFWI\\ASL\\INPUT\\STATIC\\IFWI_UNZIP
+cd %WORKING_DIR%\\IFWI_Automation\\Unified_IFWI\\Builder
 py one_darzi.py ASL
