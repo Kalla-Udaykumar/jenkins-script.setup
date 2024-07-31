c4697eb0ca928c0b09462c3d44dd8e31d33c9c58
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2024-07-31 10:42:44 +0530

    Delete asl-unified-odm directory

--- asl-unified-odm/idf/build_odm.bat
+++
@@ -1,5 +0,0 @@
-echo workspace is: %WORKSPACE%
-echo working_dir is: %WORKING_DIR%
-copy /Y %WORKSPACE%\\abi\\BIOS_Manifest.json %WORKING_DIR%\\IFWI_Automation\\Unified_IFWI\\ASL\\INPUT\\STATIC\\IFWI_UNZIP
-cd %WORKING_DIR%\\IFWI_Automation\\Unified_IFWI\\Builder
-py one_darzi.py ASL
