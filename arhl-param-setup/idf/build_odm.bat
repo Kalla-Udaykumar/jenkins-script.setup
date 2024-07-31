935ea082db2993b5c990bc7b7dd5f397d08a8815
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2024-07-31 10:41:50 +0530

    Delete arhl-param-setup directory

--- arhl-param-setup/idf/build_odm.bat
+++
@@ -1,7 +0,0 @@
-echo workspace is: %WORKSPACE%
-echo working_dir is: %WORKING_DIR%
-cd %WORKING_DIR%\\IFWI_Automation\\ARL\\test\\
-py ARL_IFWI_download.py ARL_H %IFWI_VERSION% Pre_Production
-copy /Y %WORKSPACE%\\abi\\BIOS_Manifest.json %WORKING_DIR%\\IFWI_Automation\\ARL\\ARL_H\\INPUT\\STATIC\\IFWI_UNZIP
-cd %WORKING_DIR%\\IFWI_Automation\\ARL\\test\\
-py ARL_one_darzi.py ARL_H
