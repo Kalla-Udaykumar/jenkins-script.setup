708e755ec7d903633ea6abc005e2af9f84e7cf2b
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2024-08-22 07:57:54 +0530

    Update Jenkinsfile.groovy

--- 66788/jammy/Jenkinsfile.groovy
+++ 66788/jammy/Jenkinsfile.groovy
@@ -629,9 +629,7 @@ pipeline{
         }
         stage('upload to user repo') {
             when {
-                expression {
-                    { params.USER_UPLOAD == true }
-                }
+                expression { params.USER_UPLOAD == true }
             }
 
             steps{
