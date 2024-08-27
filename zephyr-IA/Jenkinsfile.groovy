fe1843d8c5235ed0d06c67e801415598d9bb5111
kumarnac <nachiketa.kumar@intel.com>
2024-08-27 10:15:41 +0530

    Update Jenkinsfile.groovy

--- zephyr-IA/Jenkinsfile.groovy
+++ zephyr-IA/Jenkinsfile.groovy
@@ -8,7 +8,7 @@ def Protex_IA_Files() {
  	mkdir  -p  "${WORKSPACE}"/Protex_IA/zephyr/drivers/ith/
 	cp -r "${WORKSPACE}"/zephyr/drivers/ith "${WORKSPACE}"/Protex_IA/zephyr/drivers/ith
  	mkdir -p  "${WORKSPACE}"/Protex_IA/zephyr/subsys/tracing/ith
-        cp -r "${WORKSPACE}"/zephyr/drivers/ith "${WORKSPACE}"/Protex_IA/zephyr/subsys/tracing/ith
+        cp -r "${WORKSPACE}"/zephyr/subsys/tracing/ith "${WORKSPACE}"/Protex_IA/zephyr/subsys/tracing/ith
 	"""
     }
 }
